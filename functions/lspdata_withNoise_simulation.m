e384e609b52adea0285e9e38820f2fb00f24655c
Rachel <rachelemja@gmail.com>
2020-12-29 09:12:27 +0100

    notation consistent with published paper

--- functions/lspdata_withNoise_simulation.m
+++ functions/lspdata_withNoise_simulation.m
@@ -1,4 +1,4 @@
-function [X,C,R,Q]=lspdata_withNoise_simulation(num_sim, a_q, b_q, c_q, c_r,sigma_square,N,time);
+function [X,C,R,Q]=lspdata_withNoise_simulation(num_sim,a_q, b_q, c_q, c_r,sigma_square,N,time);
 
 % LSPDATA computes num_sim data-vector of a locally stationary process 
 % with the model parameters specified, each trajectory of length N 
@@ -19,13 +19,13 @@ function [X,C,R,Q]=lspdata_withNoise_simulation(num_sim, a_q, b_q, c_q, c_r,sigm
 %
 
 t = time;
-s=t';
+s = t';
 
 tau_R = t*ones(1,length(s))-ones(length(t),1)*s;
 tau_Q = (t*ones(1,length(s))+ones(length(t),1)*s)./2;
 
 R = exp(-(c_r/8).*(tau_R).^2);
-Q = sigma_square + a_q * exp(-(c_q/2).*((tau_Q-b_q).^2));
+Q = sigma_square+ a_q*exp(-(c_q/2).*((tau_Q-b_q).^2));
 
 % Covariance matrix of the base-band lsp-process
 C=(R.*Q);
